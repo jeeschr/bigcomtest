88c4af6fd120f0b91f4e374eb9db9686518e816a
jeeschr <jeschror@gmail.com>
2021-02-15 19:53:14 -0500

    first commit

---
+++ assets/js/theme/custom.js
@@ -0,0 +1,97 @@
+import PageManager from "./page-manager";
+import swal from './global/sweet-alert';
+
+export default class Custom extends PageManager {
+
+	onReady(){
+		console.log("Hello world!");
+
+		var products = this.context.products;
+
+		console.log('testing: ',products);
+
+		// add all items to cart button
+		// 
+		$('#form-action-addToCart').on('click', event => {
+			console.log('add to cart');
+			event.preventDefault();
+
+		    const $addToCartBtn = $('#form-action-addToCart', $(event.target));
+            const waitMessage = $addToCartBtn.context.data('waitMessage');
+
+
+		    $addToCartBtn.context
+			    .val(waitMessage)
+                .prop('disabled', true);
+
+
+			fetch('/api/storefront/cart', {
+				credentials: 'include'
+			}).then(function(response) {
+				// console.log('get cart', response.json());
+				return response.json();
+			})
+			.then(function(reponse) {
+				products.forEach(function(product){
+
+					console.log('prodcuts',product.id);
+
+					$.get("/cart.php?action=add&product_id="+product.id);
+
+				});
+
+
+	            swal.fire({
+	                text: 'All items added to cart',
+	                icon: 'warning',
+	                showCancelButton: false,
+	            }).then((result) => {
+	                if (result.value) {
+	                    window.location.reload();
+	                }
+	            });
+
+				
+			});
+
+		});
+
+		// cart remove button
+		// 
+        $('#cart-remove').on('click', event => {
+
+            console.log('cart remove');
+            event.preventDefault();
+
+            fetch('/api/storefront/cart', {
+
+             	credentials: 'include'
+
+            }).then(function(response) {
+
+             	return response.json();
+
+            })
+            .then(function(myJson) {
+
+                fetch('/api/storefront/carts/'+myJson[0].id, {
+                    method: 'DELETE',
+                });
+
+            });
+
+            swal.fire({
+                text: 'All items removed from cart',
+                icon: 'warning',
+                showCancelButton: false,
+            }).then((result) => {
+                if (result.value) {
+                    window.location.reload();
+                }
+            });
+
+        });
+
+	}
+
+}
